88947834c1b8579816e77f89ffc9d4b7dad1a004
dhanugiri007 <dhanashrichaudhari200@gmail.com>
2026-05-21 15:40:56 +0530

    mistakes improved in service and user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -39,8 +39,6 @@ userSchema.pre("save", async function () {
 
 userSchema.methods.comparePassword = async function (password) {
 
-    console.log(password, this.password)
-
     return await bcrypt.compare(password, this.password)
 
 }
