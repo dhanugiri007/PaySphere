8fc18ad3f00bb92e242baf218f885d664286db2d
dhanugiri007 <dhanashrichaudhari200@gmail.com>
2026-05-22 15:12:08 +0530

    added frontend

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const cookie = require('cookie-parser');
 const authRouter = require('./routes/auth.route');
 const accountRouter = require('./routes/account.route');
@@ -8,10 +9,11 @@ const app = express();
 
 app.use(express.json());
 app.use(cookie());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-    res.send("Paysphere is running");
-});
+app.use(express.static(path.join(__dirname, '../public')));
+
+
 
 app.use("/api/auth",authRouter);
 app.use("/api/account",accountRouter);
