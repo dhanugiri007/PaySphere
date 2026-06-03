ebb83b7bd8a8e042bfbf88e33f1fe33cad646bee
dhanugiri007 <dhanashrichaudhari200@gmail.com>
2026-06-03 14:53:07 +0530

    done

--- src/services/swagger.js
+++ src/services/swagger.js
@@ -10,7 +10,7 @@ const options = {
     },
     servers: [
       {
-        url: "https://paysphere-ih0s.onrender.com/",
+        url: "https://paysphere-ih0s.onrender.com",
         description: "production server",
       },
        {
